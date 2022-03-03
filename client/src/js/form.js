923b487b181561721ae49dcdd1ebe0ac0edd158c
Carlos Costoso <ccostoso@c02gq142q05q.2tor.net>
2022-03-03 15:13:52 -0500

    Gitignored package-lock.json

---
+++ client/src/js/form.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("formToggle");
+
+const toggleForm = () => {
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
+}
+
+const newContactButton = document.getElementById("new-contact");
+
+newContactButton.addEventListener('click', event => {
+ toggleForm()
+})
